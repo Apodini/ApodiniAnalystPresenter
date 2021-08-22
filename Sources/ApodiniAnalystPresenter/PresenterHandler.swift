09e0cfb993d7f3ba320cebc388c1bdf555fe027d
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2021-08-22 18:56:43 +0200

    Update to Apodini 0.4.0, add REUSE conformance, and add documentation (#1)

--- Sources/ApodiniAnalystPresenter/PresenterHandler.swift
+++ Sources/ApodiniAnalystPresenter/PresenterHandler.swift
@@ -1,7 +1,16 @@
+//
+// This source file is part of the Apodini AnalystPresenter open source project
+//
+// SPDX-FileCopyrightText: 2021 Paul Schmiedmayer and the project authors (see CONTRIBUTORS.md) <paul.schmiedmayer@tum.de>
+//
+// SPDX-License-Identifier: MIT
+//
+
 import Apodini
 import Logging
 
 
+/// A `PresenterHandler` provides a `Handler` that returns the encoded view of the ``PresenterService`` stored in the `@Environment`
 public struct PresenterHandler: Handler {
     @Apodini.Environment(\.presenterService) var presenterService: PresenterService
     @Apodini.Environment(\.logger) var logger: Logger
@@ -10,15 +19,16 @@ public struct PresenterHandler: Handler {
     var serverError: ApodiniError
     
     
+    /// Creates a new `PresenterHandler`.
     public init() {}
     
     
-    public func handle() -> EventLoopFuture<Blob> {
-        presenterService
-            .encodedView
-            .flatMapErrorThrowing { error in
-                logger.error("\(error)")
-                throw serverError
-            }
+    public func handle() async throws -> Blob {
+        do {
+            return try await presenterService.encodedView
+        } catch {
+            logger.error("\(error)")
+            throw serverError
+        }
     }
 }
